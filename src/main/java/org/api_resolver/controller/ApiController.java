25628a4687ce7ca3a1611be81890a3b4e76cac0f
mehmet kirimli <kirimlimehmet@gmail.comm>
2024-11-20 16:29:55 +0300

    ifx

--- src/main/java/org/api_resolver/controller/ApiController.java
+++ src/main/java/org/api_resolver/controller/ApiController.java
@@ -3,6 +3,7 @@ package org.api_resolver.controller;
 import lombok.RequiredArgsConstructor;
 import org.api_resolver.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class ApiController
     private final TransactionReportService transactionReportService;
     private final TransactionService transactionService;
 
-    @PostMapping(value = "/login")
+    @GetMapping(value = "/login")
     public String login()
     {
         return loginService.login();
