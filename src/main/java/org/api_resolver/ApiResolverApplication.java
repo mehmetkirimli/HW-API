6eb6ba4073a73dc2d0adfc4d1759c738ce30793b
mehmet kirimli <kirimlimehmet@gmail.comm>
2024-11-20 10:53:04 +0300

    First Commit Create Api - Resolver Project

---
+++ src/main/java/org/api_resolver/ApiResolverApplication.java
@@ -0,0 +1,13 @@
+package org.api_resolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/api_resolver/ApiResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api_resolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
